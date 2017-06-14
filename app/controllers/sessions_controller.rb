b58ff06d93fe9d59322df9c413fa0fbf2d64357d
Manasvi kumar <manasvi.kumar@weboniselab.com>
2017-06-14 22:02:06 +0530

    corrections

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,5 @@
 class SessionsController < ApplicationController
-  
+
   def new
   end
 
@@ -7,7 +7,8 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       log_in user
-      redirect_to user      # Log the user in and redirect to the user's show page.
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+      redirect_to user
     else
       flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
