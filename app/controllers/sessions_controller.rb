96b302cc6d954e29f8657f967ca345cf2ea2c85c
Manasvi kumar <manasvi.kumar@weboniselab.com>
2017-06-14 21:27:58 +0530

    Revert "Implemeneted Advanced login"
    This reverts commit 036dea3ec0336550b57a723f569bb217f863068f.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,6 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       log_in user
-      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
       redirect_to user      # Log the user in and redirect to the user's show page.
     else
       flash.now[:danger] = 'Invalid email/password combination'
@@ -16,7 +15,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    log_out if logged_in?
+    log_out
     redirect_to root_url
   end
 end
